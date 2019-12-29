5bca42e6c55d22d7467f013de9d8a9d6ba03e797
SelimAbidin <ysabidin@gmail.com>
2019-12-29 22:57:48 +0300

    part 2

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -7,22 +7,47 @@ fn calc_req_fuel(mass: i32) -> i32 {
     return missed;
 }
 
+fn calc_real_req_fuel(mass: i32) -> i32 {
+    let mut fuel: i32 = calc_req_fuel(mass);
+    let mut total: i32 = 0;
+    while fuel > 0 {
+        total += fuel;
+        fuel = calc_req_fuel(fuel);
+    }
+    return total;
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let filename: String = String::from("input.txt");
     let contents: String = fs::read_to_string(filename)?;
-    let mut lines: Lines = contents.lines();
-    let deneme: i32 = lines
-        .map(|s| s.parse().unwrap())
+    let lines: Lines = contents.lines();
+
+    let numbers = lines.map(|s| s.parse().unwrap());
+
+    let result1: i32 = numbers
+        .clone()
         .map(calc_req_fuel)
         .fold(0, |acc: i32, curr: i32| acc + curr);
-    println!("With text:\n{:?}", deneme);
+
+    let result2: i32 = numbers
+        .map(calc_real_req_fuel)
+        .fold(0, |acc: i32, curr: i32| acc + curr);
+    println!("Part 1 :\n{:?}", result1);
+    println!("Part 2 :\n{:?}", result2);
     Ok(())
 }
 
 #[test]
-fn another() {
+fn test_calc_req_fuel() {
     assert_eq!(calc_req_fuel(12), 2);
     assert_eq!(calc_req_fuel(14), 2);
     assert_eq!(calc_req_fuel(1969), 654);
     assert_eq!(calc_req_fuel(100756), 33583);
+}
+
+#[test]
+fn test_calc_real_req_fuel() {
+    assert_eq!(calc_real_req_fuel(14), 2);
+    assert_eq!(calc_real_req_fuel(1969), 966);
+    assert_eq!(calc_real_req_fuel(100756), 50346);
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+
+Codding challange with Rust
+https://adventofcode.com/2019
+
+
+### Read about it
+https://adventofcode.com/2019/about
